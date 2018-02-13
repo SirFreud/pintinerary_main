cde1b99f98affb2eb8b0b76412a521a2e3ba011b
Sirfreud <mikevaldez92@gmail.com>
2018-02-13 16:57:50 -0600

    Made Item model, migration and controller

---
+++ database/migrations/2018_02_13_225622_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('itinerary_id')->index();
+            $table->string('item_name');
+            $table->text('notes');
+            $table->integer('price', 6, 2);
+            $table->time('time');
+            $table->integer('day_number');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
